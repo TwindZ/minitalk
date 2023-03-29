83b12781a1a767f43976502a0c4599b15574f038
TwindZ <emmanuel.lamothe@gmail.com>
2023-03-29 16:55:18 -0400

    NoGlobal w/ pingpong work

--- NoGlobal/client.c
+++ NoGlobal/client.c
@@ -6,55 +6,83 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 12:43:07 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/03/27 16:10:32 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/03/29 16:52:58 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_handle_sig(int sig)
+static t_data	*ft_init_struct(char *str, int server_pid)
 {
-	if (sig == SIGUSR1)
-		ft_putstr("\n\e[32mHouston, touch down !\e[0m\n\n");
-	exit(0);
+	static t_data *data;
+	
+	if(!data)
+	{
+		data = malloc(sizeof(t_data));
+		data->str = str;
+		data->free_flag = 0;
+		data->i = 0;
+		data->j = 32;
+		data->len = ft_strlen(str);
+		data->octet = data->len;
+		data->si_pid = server_pid;
+	}
+	return(data);
 }
 
-static void	ft_encode(unsigned int octets, int typesize, int pid)
+static void	ft_encode(t_data *data)
 {
-	while (typesize)
+	data->j--;
+	if ((data->octet >> data->j & 1) == 0)
+			kill(data->si_pid, SIGUSR1);
+	else
+			kill(data->si_pid, SIGUSR2);
+		
+	if (data->j == 0)
 	{
-		usleep(50);
-		typesize--;
-		if ((octets >> typesize & 1) == 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
+		data->j = 8;
+		data->octet = data->str[data->i];
+		data->i++;
 	}
+}
+
+static void	ft_sa_handler(int sig)
+{
+	t_data *data;
+
+	data = ft_init_struct(NULL, 0);
+	if (sig == SIGUSR1 && data->i <= data->len + 1)
+	{
+			ft_encode(data);
+	}
+	else if (sig == SIGUSR2)
+	{
+		ft_putstr("\n\e[32mHouston, touch down !\e[0m\n\n");
+		free(data);
+		exit(0);
+	}
+		
 }
 
 int	main(int argc, char **argv)
 {
-	int					serverpid;
-	int					i;
 	struct sigaction	sa;
+	t_data	*data;
 
-	i = 0;
-	sa.sa_handler = &ft_handle_sig;
+	sa.sa_handler = &ft_sa_handler;
 	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	if (argc == 3)
 	{
-		serverpid = ft_atoi(argv[1]);
-		if (kill(serverpid, 0) < 0)
+		data = ft_init_struct(argv[2], ft_atoi(argv[1]));
+		if (kill(data->si_pid, 0) < 0)
 		{
 			ft_putstr("\n\e[31mInvalid server PID\e[0m\n\n");
 			exit(-1);
 		}
-		ft_encode(ft_strlen(argv[2]), 32, serverpid);
-		while (argv[2][i])
-			ft_encode(argv[2][i++], 8, serverpid);
-		ft_encode(argv[2][i], 8, serverpid);
-		usleep(ft_strlen(argv[2]) * 10000);
-		ft_putstr("\n\e[31mHouston, we have a problem...\e[0m\n\n");
+		kill(getpid(), SIGUSR1);
+		while(1)
+			pause();
 	}
 	else
 		ft_putstr("\e[31mWrong format ./client <server PID> \"<string>\"\e[0m\n");
--- NoGlobal/server.c
+++ NoGlobal/server.c
@@ -6,18 +6,21 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 12:43:05 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/03/27 17:10:18 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/03/29 16:51:06 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_dump(t_data *data)
+static void	ft_dump(t_data *data)
 {
 	if (data->i == data->len * 8 + 40)
 	{
 		ft_putstr(data->str);
-		kill(data->si_pid, SIGUSR1);
+		ft_putstr("\e[32m\n");
+		ft_putnbr(data->i);
+		ft_putstr(" bits received !\nReady to receive again !\n\e[0m");
+		kill(data->si_pid, SIGUSR2);
 	}
 	data->i = 0;
 	data->j = 0;
@@ -26,31 +29,12 @@ void	ft_dump(t_data *data)
 	if (data->free_flag == 1)
 	{
 		free(data->str);
-		// free(data);
+		//free(data); //pas capable de free la deuxieme fois comme si la struct ne se ft_init pas
+		data->free_flag = 0;
 	}
-	data->free_flag = 0;
 }
 
-void	ft_store_str(t_data *data)
-{
-	if (data->free_flag == 0)
-	{
-		data->str = malloc((data->len + 1) * sizeof(char));
-		if (!data->str)
-			exit(-1);
-		data->free_flag = 1;
-	}
-	if (data->octet == 0)
-	{
-		ft_dump(data);
-		return ;
-	}
-	data->str[data->j++] = data->octet;
-	data->octet = 0;
-	data->i = 0;
-}
-
-t_data	*ft_init_struct(void)
+static t_data	*ft_init_struct(void)
 {
 	static t_data *data;
 	
@@ -69,14 +53,33 @@ t_data	*ft_init_struct(void)
 	return(data);
 }
 
-void	ft_handle_sig(int sig, siginfo_t *info, void *ucontext)
+static void	ft_store_str(t_data *data)
+{
+	if (data->free_flag == 0)
+	{
+		data->str = malloc((data->len + 1) * sizeof(char));
+		if (!data->str)
+			exit(-1);
+		data->free_flag = 1;
+	}
+	if (data->octet == 0)
+	{
+		ft_dump(data);
+		return ;
+	}
+	data->str[data->j++] = data->octet;
+	data->octet = 0;
+}
+
+
+static void	ft_sa_sigaction(int sig, siginfo_t *info, void *ucontext)
 {
 	t_data	*data;
 
 	data = ft_init_struct();
 
 	(void) ucontext;
-	if (data->si_pid != info->si_pid)
+	if (data->si_pid != info->si_pid && info->si_pid != 0)
 	{
 		if (data->free_flag == 1)
 			ft_dump(data);
@@ -90,10 +93,10 @@ void	ft_handle_sig(int sig, siginfo_t *info, void *ucontext)
 	{
 		data->len = data->octet;
 		data->octet = 0;
-		data->i = 0;
 	}
-	else if (data->i == 8 && data->len > 0)
+	else if (data->i > 32 && data->i % 8 == 0)
 		ft_store_str(data);
+	kill(data->si_pid, SIGUSR1);
 }
 
 int	main(void)
@@ -101,7 +104,7 @@ int	main(void)
 	int					pid;
 	struct sigaction	sa;
 	
-	sa.sa_sigaction = &ft_handle_sig;
+	sa.sa_sigaction = &ft_sa_sigaction;
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
